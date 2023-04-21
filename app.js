f0fcc658aa66c195b637823084890ee9403b11ea
davidmd98 <davidmd98@gmail.com>
2023-04-21 23:13:05 +0200

    Prices, header, footer, and main layer done

--- app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLink = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLink.classList.toggle('acive');
-})
